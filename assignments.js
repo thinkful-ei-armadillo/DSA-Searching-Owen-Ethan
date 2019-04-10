cb9c2cf57a4ecbffc429914d8adaec349f7df866
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-10 15:25:13 -0400

    WIP on Searching in a BST

--- assignments.js
+++ assignments.js
@@ -8,3 +8,52 @@
 
 // Find 16
 // 11, 15, 17, -1
+
+
+
+
+
+
+
+
+// Searching in a BST
+
+// in order (left, center, right)
+// 14 15 19 25 27 35 79 89 90 91
+
+// pre order (center, left, right)
+// 35 25 15 14 19 27 89 79 91 90
+
+const preOrderTraversal = [35, 25, 15, 14, 19, 27, 89, 79, 91, 90];
+
+
+const traverse = function (arr, start, end) {
+
+  if (start >= end) {
+    console.log(arr[start]);
+    return;
+  }
+
+  const node = arr[start];
+
+
+  // cal indexes for left
+  let leftStart = start + 1;
+  let leftEnd   = leftStart;
+
+  while (arr[leftEnd] < node) {    // maybe <=
+    leftEnd++;
+  }
+  traverse(arr, leftStart, leftEnd);
+
+  // cal indexes for right
+
+  let rightStart = leftEnd + 1;
+  let rightEnd   = end;
+  traverse(arr, rightStart, rightEnd);
+
+  console.log(node);
+
+};
+
+traverse(preOrderTraversal, 0, preOrderTraversal.length-1);
